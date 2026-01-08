53aa4ce19ad1ae0ad00c4d44cb9686d26c6124c8
Md Tarikul Islam <tarikulislam6004@gmail.com>
2026-01-08 19:08:35 +0600

    settings add

--- includes/admin/settings.php
+++ includes/admin/settings.php
@@ -16,7 +16,39 @@ class Settings{
     }
 
     public static function set_default_data(){
-        $data = [];
+        $users = wp_get_current_user();
+        $data = [
+            // general
+            'carbooking_currency' => [
+                'US Dollar',
+                'British Pound',
+                'Bangladeshi Taka',
+                'Indian Rupee'
+            ],
+            'carbooking_symbol_position' => [
+                'Before',
+                'After'
+            ],
+            'carbooking_base_pricing_unit' => [
+                'Daily (Fixed 24h cycles)',
+                'Hourly (Flexable duration)'
+            ],
+            'carbooking_min_days' => '1',
+            'carbooking_min_driver_age' => '21',
+            'carbooking_daily_opration_from' => '',
+            'carbooking_daily_opration_to' => '',
+            'carbooking_before_buffer_time' => '',
+            'carbooking_allow_weekend_day' => 'boolean',
+            // payments
+            'carbooking_book_now_pay_later' => 'boolean',
+            'carbooking_direct_bank_transfer' => 'boolean',
+            'carbooking_cash_on_picup' => 'boolean',
+            // email
+            'carbooking_admin_email' => $users->user_email,
+            'carbooking_brand_logo_url' => '',
+            'carbooking_email_footer_text' => '',
+            'carbooking_picup_&_drop_location' => [],
+        ];
 
         return apply_filters('carbooking/admin/settings_default_data', $data);
     }
